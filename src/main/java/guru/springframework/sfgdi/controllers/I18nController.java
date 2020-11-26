06af7920c2ee18997fd1091c119418da94b1889c
konta94 <kontaivan@gmail.com>
2020-11-26 22:39:53 +0100

    Internationalization Controller (EN and ES). Using Spring Profile().

--- src/main/java/guru/springframework/sfgdi/controllers/I18nController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/I18nController.java
@@ -1,19 +1,20 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class ConstructorInjectedController {
+public class I18nController {
+    //internationalization Controller
+
     private final GreetingService greetingService;
 
-    //@Autowired is optional for Constructors!
-    public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
+    public I18nController(@Qualifier("i18nService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
-    public String getGreeting(){
+
+    public String sayHello(){
         return greetingService.sayGreeting();
     }
 }
